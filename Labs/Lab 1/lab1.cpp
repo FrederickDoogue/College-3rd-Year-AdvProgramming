135195e0c116bfcf854a1e246627e73cda2b322c
FrederickDoogue <frederickdoogue@hotmail.com>
2024-01-24 15:45:12 +0000

    Add files via upload

---
+++ Labs/Lab 1/lab1.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+
+bool isLeapYear(int year)
+{
+	return true;
+}
+
+int Reversed(int testNumber)
+{
+	return 0;
+}
+bool isAPalindrome(int testNumber)
+{
+	return false;
+}
+bool isAPrimeNumber(int numbertoTest)
+{
+	return false;
+}
+int input5CharsConvertToInt()
+{
+	int returnInt = 0;
+	char inputChar;
+	for (int i = 0; i < 5; i++)
+	{
+		std::cin >> inputChar;
+		//check if its a digit.
+		//do something
+
+	}
+	return returnInt;
+}
+int convertBinarytoDecimal(int binaryNumber)
+{
+	return 0;
+}
+void drawRightAngledTriangle()
+{
+
+}
+void drawIsocelesTriangle()
+{
+
+}
+void drawIsocelesTriangle2()
+{
+
+}
+// changing anything below here equals 0 marks!!!!!!!!!
+
+int main()
+{
+	std::cout << "Leapyear" << std::endl;
+	if (!isLeapYear(1700))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (isLeapYear(1600))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (isLeapYear(2016))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	std::cout << "NumberReversed" << std::endl;
+	if (Reversed(123) == 321)
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+
+	if (Reversed(1) == 1)
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (Reversed(1234) != 321)
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+
+	std::cout << "Palindrome" << std::endl;
+	if (isAPalindrome(1))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (isAPalindrome(121))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (!isAPalindrome(1213))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	std::cout << "Prime Number" << std::endl;
+	if (isAPrimeNumber(3))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (isAPrimeNumber(2147483647))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (!isAPrimeNumber(4))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	std::cout << "Binary to Decimal" << std::endl;
+	if (convertBinarytoDecimal(110) == 6)
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (convertBinarytoDecimal(111) == 7)
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	std::cout << "5 chars to int" << std::endl;
+	int convertedInt = input5CharsConvertToInt();
+	std::cout << convertedInt << std::endl;
+
+	std::cout << "Triangles" << std::endl;
+	drawRightAngledTriangle();
+	drawIsocelesTriangle();
+	std::cout << "Triangle Extra" << std::endl;
+	drawIsocelesTriangle2();
+
+}
